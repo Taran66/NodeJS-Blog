28b9a53d3dd9a03d9e6f15d8ea4bb7a452f57edc
Taran66 <taranpreet1911@gmail.com>
2024-06-06 15:47:29 +0530

    initial commit

---
+++ server/routes/main.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router();
+
+router.get('', (req, res) => {
+
+    const locals = {
+        title: "NodeJs Blog",
+        description: "Simple Blog created with Nodejs, Express and MongoDb"
+    }
+
+    res.render('index', { locals });
+});
+
+router.get('/about', (req, res) => {
+    res.render('about');
+});
+
+
+module.exports = router;
