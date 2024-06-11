b5694c9d0fa2f06de045fd01eeb7a117886780aa
Taran66 <taranpreet1911@gmail.com>
2024-06-11 06:38:34 +0530

    updated routes server and views

--- server/routes/main.js
+++ server/routes/main.js
@@ -31,6 +31,59 @@ router.get('', async (req, res) => {
     }
 });
 
+
+router.get('/post/:id', async (req, res) => {
+  try {
+      let slug = req.params.id;
+
+    const data = await Post.findById({ _id: slug });
+
+    const locals = {
+        title: data.title,
+        description: "Simple Blog created with NodeJs, Express & MongoDb."
+    }
+
+    res.render('post', { locals, data });
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+
+
+
+router.post('/search', async (req, res) => {
+  
+
+  try {
+    const locals = {
+        title: "NodeJs Blog",
+        description: "Simple Blog created with NodeJs, Express & MongoDb."
+      }
+
+
+      let searchTerm = req.body.searchTerm;
+      const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "")
+
+
+    const data = await Post.find({
+        $or: [
+            { title: {$regex: new RegExp(searchNoSpecialChar, 'i')} },
+            { body: {$regex: new RegExp(searchNoSpecialChar, 'i')} },
+
+        ]
+    });
+    res.render("search", {
+        data, 
+        locals
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
 // function insertPostData (){
 //     Post.insertMany([
 //         {
